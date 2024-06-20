c8114ced3cf57c17628bdafc234f1f011eb6aeb1
Gabriel Spacki <ifprgabriel@gmail.com>
2024-06-19 21:39:05 -0300

    ✨ feat: added models: - Cliente - Modulo - Ticket

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    class Meta:
+        db_table = 'CLIENTE'
+
+    id = models.AutoField(primary_key = True, db_column = 'ID')
+    nome = models.CharField(max_length = 255, db_column = 'NOME')
+
+class Modulo(models.Model):
+    class Meta:
+        db_table = 'MODULO'
+
+    id = models.AutoField(primary_key = True, db_column = 'ID')
+    nome = models.CharField(max_length = 255, db_column = 'NOME')
+
+class Ticket(models.Model):
+    class Meta:
+        db_table = 'TICKET'
+
+    id = models.AutoField(primary_key = True, db_column = 'ID')
+    titulo = models.CharField(max_length = 255, db_column = 'TITULO')
+    cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE, db_column = 'CODCLIENTE')
+    modulo = models.ForeignKey(Modulo, on_delete = models.CASCADE, db_column = 'CODMODULO')
+    data_abertura = models.DateTimeField(auto_now_add = True, db_column = 'DATAABERTURA')
+    data_encerramento = models.DateTimeField(auto_now_add = True, db_column = 'DATAENCERRAMENTO')
